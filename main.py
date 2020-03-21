9db394490d911d12854dc479b1059fdafbc93254
hui0808 <laozhihui8@outlook.com>
2020-03-21 23:55:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import re
 import math
+from enum import Enum
+
+
+class TokenEnum(Enum):
+    num = 1
+    newline = 2
+    skip = 3
+    lparen = 4
+    rparen = 5
+    compopr = 6
+    binopr = 7
+    const = 8
+    mismatch = 9
 
 
 class Token():
-    operator = 'operator'
-    compopr = 'compopr'
-    num = 'num'
-    newline = 'newline'
-    skip = 'skip'
-    lparen = 'lparen'
-    rparen = 'rparen'
-    binopr = 'binopr'
-    mismatch = 'mismatch'
 
     def __init__(self, type, value, line, column):
         self.type = type
@@ -22,39 +26,40 @@ class Token():
     def __repr__(self):
         return f"Token(type={self.type}, value={self.value!r}, line={self.line}, column={self.column})"
 
-
-def tokenize(code):
-    """
-    词法分析
-    """
-    token_specification = [
-        ('num', r'\d+(\.\d*)?'),  # 整数和小数
-        ('newline', r'\n'),  # 回车
-        ('skip', r'[ \t]+'),  # 跳过所有空格以及缩进
-        ('lparen', r'\('),  # 左圆括号
-        ('rparen', r'\)'),  # 右圆括号
-        ('compopr', r'(==|!=|>=|<=|>|<)'),  # 比较操作符
-        ('binopr', r'(\+|-|\*|/|\^)'),  # 二元操作符
-        ('mismatch', r'\S+'),  # 其他未匹配的词
-    ]
-    tok_regex = r'|'.join('(?P<%s>%s)' % pair for pair in token_specification)
-    r = re.compile(tok_regex)
-    line_num = 1
-    line_start = 0
-    for mo in r.finditer(code):
-        kind = mo.lastgroup
-        value = mo.group()
-        column = mo.start() - line_start
-        if kind == 'num':
-            value = float(value) if '.' in value else int(value)
-        elif kind == 'newline':
-            line_start = mo.end()
-            line_num += 1
-        elif kind == 'skip':
-            continue
-        elif kind == 'mismatch':
-            raise Exception(f'{value!r} unexpected on line {line_num}')
-        yield Token(kind, value, line_num, column)
+    @staticmethod
+    def tokenize(code):
+        """
+        词法分析
+        """
+        token_specification = [
+            (TokenEnum.num, r'\d+(\.\d*)?'),  # 整数和小数
+            (TokenEnum.newline, r'\n'),  # 回车
+            (TokenEnum.skip, r'[ \t]+'),  # 跳过所有空格以及缩进
+            (TokenEnum.lparen, r'\('),  # 左圆括号
+            (TokenEnum.rparen, r'\)'),  # 右圆括号
+            (TokenEnum.compopr, r'(==|!=|>=|<=|>|<)'),  # 比较操作符
+            (TokenEnum.binopr, r'(\+|-|\*|/|\^)'),  # 二元操作符
+            (TokenEnum.const, r'(exp|pi|tau)'),  # 常量
+            (TokenEnum.mismatch, r'\S+'),  # 其他未匹配的词
+        ]
+        tok_regex = r'|'.join(f'(?P<{pair[0].name}>{pair[1]})' for pair in token_specification)
+        r = re.compile(tok_regex)
+        line_num = 1
+        line_start = 0
+        for mo in r.finditer(code):
+            kind = mo.lastgroup
+            value = mo.group()
+            column = mo.start() - line_start
+            if kind == 'num':
+                value = float(value) if '.' in value else int(value)
+            elif kind == 'newline':
+                line_start = mo.end()
+                line_num += 1
+            elif kind == 'skip':
+                continue
+            elif kind == 'mismatch':
+                raise Exception(f'{value!r} unexpected on line {line_num}')
+            yield Token(TokenEnum[kind], value, line_num, column)
 
 
 class Parser():
@@ -63,33 +68,33 @@ class Parser():
         self.tokens = None
         self.lookahead = None
 
+    def error(self):
+        raise Exception(f'syntex error, {self.code[:self.lookahead.column] + "?" + self.code[self.lookahead.column:]}')
+
     def exec(self, code):
         self.code = code
-        self.tokens = tokenize(code)
+        self.tokens = Token.tokenize(code)
         try:
             self.lookahead = next(self.tokens)
         except StopIteration:
             return None
-        return self.A()
+        value = self.A()
+        if self.tokens is not None:
+            self.error()
+        return value
 
     def match(self, t):
-        if t == self.lookahead.value:
+        if self.tokens is None:
+            self.error()
+        elif t == self.lookahead.value:
             try:
-                if self.tokens is None:
-                    raise Exception(
-                        f'syntex error, {self.code[:self.lookahead.column] + "?" + self.code[self.lookahead.column:]}')
                 self.lookahead = next(self.tokens)
             except StopIteration:
                 self.tokens = None
 
     def A(self):
         lvalue = self.B()
-        lvalue = self.A_tail(lvalue)
-        if self.tokens is not None:
-            raise RuntimeError(
-                f'syntex error, {self.code[:self.lookahead.column] + "?" + self.code[self.lookahead.column:]}')
-        else:
-            return lvalue
+        return self.A_tail(lvalue)
 
     def A_tail(self, lvalue):
         if self.lookahead.value == '+':
@@ -145,7 +150,7 @@ class Parser():
             value = self.A()
             self.match(')')
             return value
-        elif self.lookahead.type == Token.num:
+        elif self.lookahead.type == TokenEnum.num:
             value = self.lookahead.value
             self.match(value)
             return value
